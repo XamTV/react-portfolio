a7ef21d56b7ad9d3e168431922e2f38ec63ec1d0
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-05-03 16:29:27 +0200

    Homepage 85% done, need to add Contact button a place nav and footer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,34 @@
+import Profile from "../assets/profile.jpg";
+import "../styles/HomePage.css";
+
 function HomePage() {
   return (
-    <section className="HomePageComponent">
-      <h1>HomePage</h1>
-
-      <p>HomePage Component</p>
-    </section>
+    <header className="header">
+      <section className="HomePageComponent">
+        <img src={Profile} alt="Coucou" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa dolor
+          quos consectetur architecto minus dicta vel aut facilis magnam non
+          consequuntur, commodi fugit perspiciatis fugiat. Temporibus,
+          voluptates dolorum et quam velit asperiores maiores pariatur odit quod
+          aperiam provident, nesciunt laborum omnis vel itaque voluptatibus
+          quas? Quo asperiores facere aut eum.
+        </p>
+      </section>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque dolores
+        quis accusantium sunt labore veniam accusamus, culpa magni fuga autem
+        rem reiciendis consequatur vel neque qui, odit exercitationem sit
+        aliquid corporis. Distinctio ea repellat natus perspiciatis ipsa dolore
+        quos accusantium sit. Minima assumenda qui, doloremque repudiandae enim
+        id ipsa cumque alias adipisci obcaecati temporibus magnam modi deserunt,
+        saepe architecto, non quae tempora eum? Sunt eum rem esse vero ipsam
+        quam! Eaque obcaecati cum, quam aliquam perspiciatis illum dignissimos
+        expedita similique impedit numquam dicta aliquid ducimus veritatis
+        excepturi ipsam ex voluptas, tempore deserunt enim. Quod dolores
+        excepturi illum odio fugit sapiente?
+      </p>
+    </header>
   );
 }
 
